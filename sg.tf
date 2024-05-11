d05794210348bbe122f484a42aae1411fa5b4c60
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-11 14:44:39 +0530

    auto pushhh

--- sg.tf
+++ sg.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow_rabbitmq" {
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
-    from_port        = 5672
-    to_port          = 5672
+    from_port        = var.RABBITMQ_PORT
+    to_port          = var.RABBITMQ_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,4 @@ variable "ENV" {}
 
 #RabbitMQ Variables
 variable "RABIIT_MQ_INSTANCE_TYPE" {}
+variable "RABBITMQ_PORT" {}
